83564c9f65ac915547d751910d17b9fb2237e284
豪 樹 冬 木 <fuyukihidekii@gmail.com>
2023-11-26 15:43:21 -0300

    java: re-organize device makefile
    * introduce all properties removed from twrp makefile there
    
    Change-Id: If94a474e9a2c89860f427eedadeefc9af8f0be2d
    Signed-off-by: 豪 樹 冬 木 <fuyukihidekii@gmail.com>

--- device.mk
+++ device.mk
@@ -5,13 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those AOSP products, most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from common TWRP configuration.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
 LOCAL_PATH := device/motorola/java
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
@@ -27,9 +28,28 @@ PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     libz \
     libcutils
 
+# A/B: update engine
 PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# A/B: OTA dexopt update_engine hookup
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# GMS: Partner ID
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+# Build ID
+BUILD_FINGERPRINT := motorola/java_retail/java:11/RTAS31.68-66-2/66-2:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="p352_Natv-user 11 RTAS31.68-66-2 66-2 release-keys"
